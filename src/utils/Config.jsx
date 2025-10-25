7ba845f5336b0764bade35debbe25c8499c73ec6
K Narayan Reddy <narayanreddy355@gmail.com>
2025-10-26 01:04:02 +0530

    small changes done

--- src/utils/Config.jsx
+++ src/utils/Config.jsx
@@ -1,4 +1,4 @@
-//  const  BASE_URL ="https://fms-backend-6fjl.onrender.com/api/auth"
- const  BASE_URL ="http://localhost:3000/api/auth"
+ const  BASE_URL ="https://fms-backend-6fjl.onrender.com/api/auth"
+
 
  export default BASE_URL
