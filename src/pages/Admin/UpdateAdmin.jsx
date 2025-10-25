548e9167b7423c57a32e2e5cc244eb699fe4a58b
K Narayan Reddy <narayanreddy355@gmail.com>
2025-10-26 00:13:39 +0530

    changes done

--- src/pages/Admin/UpdateAdmin.jsx
+++ src/pages/Admin/UpdateAdmin.jsx
@@ -76,8 +76,6 @@ const UpdateAdmin = () => {
         { withCredentials: true }
       );
       if (res.data?.success) {
-        console.log(res.data.student.courseFee);
-        console.log(!res.data.student.courseFee);
         
         toast.success(res.data.message || "Student updated successfully");
         navigate("/admin/students");
